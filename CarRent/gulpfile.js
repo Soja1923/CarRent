773f7b1da4eac1d4e5b9b8add5e1a60ab5a33b7b
Soja1923 <psoja1923@gmail.com>
2018-07-14 11:39:19 +0200

    Add bootstrap 4

---
+++ CarRent/gulpfile.js
@@ -0,0 +1,13 @@
+﻿var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+var cssDest = 'wwwroot/css';
+var cssInputFile = 'wwwroot/style.scss';
+
+gulp.task('buildcss', function () {
+    return gulp.src(cssInputFile)
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(gulp.dest(cssDest));
+});
