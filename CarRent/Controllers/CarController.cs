5c05ffbeccf7a67f471081d85aed2072d68f7ceb
Soja1923 <psoja1923@gmail.com>
2018-07-31 16:32:33 +0200

    front-end v1.0

---
+++ CarRent/Controllers/CarController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRent.Controllers
+{
+    public class CarController : Controller
+    {
+        public IActionResult CarsList()
+        {
+            return View();
+        }
+
+        public IActionResult Car()
+        {
+            return View();
+        }
+    }
+}
